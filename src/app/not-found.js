77ea44dfd31d9e3fdaa20b73ac4d21bbb64fb8f9
hatim77100 <najoua-hatim@hotmail.fr>
2023-11-19 17:11:41 +0100

    :goal_net:error gestion

---
+++ src/app/not-found.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const notFound = () => {
+  return (
+    <div className="error">
+      <h1>404</h1>
+      <p>Oups, la page que vous demandez n&apos;existe pas.</p>
+    </div>
+  );
+};
+
+export default notFound;
